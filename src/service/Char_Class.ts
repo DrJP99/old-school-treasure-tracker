392bf138c7d8edf2a256b39a1c15118f78fd8699
DrJP99 <jpgzz1999@gmail.com>
2024-09-12 12:26:33 -0600

    (feat): Character class, classes and TPX calculator

---
+++ src/service/Char_Class.ts
@@ -0,0 +1,10 @@
+export enum Char_Class {
+	cleric = 'cleric',
+	dwarf = 'dwarf',
+	elf = 'elf',
+	fighter = 'fighter',
+	halfling = 'halfling',
+	magic_user = 'magic-user',
+	thief = 'thief',
+	normal_human = 'normal human'
+}
