248733577fa45aaaa6133dbc13df24d1faad248d
DrJP99 <jpgzz1999@gmail.com>
2024-09-12 19:51:09 -0600

    (feat): added get methods to Monsters, plus quantity for purposes of xp calculation)

--- src/service/Monster.ts
+++ src/service/Monster.ts
@@ -2,10 +2,32 @@ export class Monster {
     private name: string
     private description: string
     private xp: number
+    private qty: number
 
-    constructor(name: string, description: string, xp: number) {
+    constructor(name: string, description: string, xp: number, qty: number) {
         this.name = name
         this.description = description
         this.xp = xp
+        this.qty = qty
+    }
+
+    public get_name = (): string => {
+        return this.name
+    }
+
+    public get_description = (): string => {
+        return this.description
+    }
+
+    public get_xp = (): number => {
+        return this.xp
+    }
+
+    public get_total_xp = (): number => {
+        return this.xp * this.qty
+    }
+
+    public get_qty = () => {
+        return this.qty
     }
 }
